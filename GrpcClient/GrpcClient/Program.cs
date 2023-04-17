bc590ac41fa1430a8858491bc6dbe3f5af88cacb
Alican <alicankilic01@hotmail.com>
2023-04-17 13:01:52 +0300

    Add project files.

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7011");//grpc service
+var client = new Greeter.GreeterClient(channel);
+
+//request
+HelloReply result = await client.SayHelloAsync(new HelloRequest
+{
+    Name = "Alican"
+});
+Console.WriteLine(result.Message);
